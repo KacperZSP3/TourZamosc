f20afbae7bd9ab28af6f38cda8bb8d890d08e44c
KacperZSP3 <k.ksykiewicz@zsp3zamosc.pl>
2024-09-29 06:47:06 -0700

    adding TourZamosc files

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace TourZamosc
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+
+    }
+
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace TourZamosc
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateLikes()
+        {
+            LikeLable.Text = $"Polubienia: {count}";
+
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+            UpdateLikes();
+           
+        }
+
+        private void OnCounterDislikeClicked(object sender, EventArgs e)
+        {
+
+            if (count > 0)
+
+            { 
+                count--;
+            
+            }
+
+            UpdateLikes();
+
+        }
+
+    }
+
+}
